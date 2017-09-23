c6257b73bd684306d585ad8404d4dd68736ea990
syx <982296740@qq.com>
2017-09-23 21:31:40 +0800

    yanghuisanjiao

--- hello.py
+++ hello.py
@@ -91,7 +91,24 @@
 # move(4, 'A', 'B', 'C')
 
 #List Comprehensions
-L=[x*x for x in range(1,11)]
-print(L)
-import os
-print([d for d in os.listdir('.')])
+# L=[x*x for x in range(1,11)]
+# print(L)
+# import os
+# print([d for d in os.listdir('.')])
+
+# L1=['Hello','World',18,'Apple',None]
+# L2=[a.lower() for a in L1 if isinstance(a,str)]
+# print(L2)
+
+# generator
+def triangles():
+    l = [1]
+    while True:
+        yield l
+        l = [1]+[l[n]+l[n+1] for n in range(len(l)-1)]+[1]
+n=0
+for t in triangles():
+	print(t)
+	n=n+1;
+	if n==10:
+		break

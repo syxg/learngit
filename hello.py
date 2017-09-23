90affcd5d2f2dd367df2bbb25f3e5912dc9b513e
syx <982296740@qq.com>
2017-09-23 20:18:59 +0800

    List Comprehensions

--- hello.py
+++ hello.py
@@ -80,12 +80,16 @@
 # print(fact(5))fgitg
 # 
 # 利用递归函数移动汉诺塔:
-def move(n, a, b, c):
-    if n == 1:
-        print('move', a, '-->', c)
-    else:
-        move(n-1, a, c, b)
-        move(1, a, b, c)
-        move(n-1, b, a, c)
+# def move(n, a, b, c):
+#     if n == 1:
+#         print('move', a, '-->', c)
+#     else:
+#         move(n-1, a, c, b)
+#         move(1, a, b, c)
+#         move(n-1, b, a, c)
 
-move(4, 'A', 'B', 'C')
+# move(4, 'A', 'B', 'C')
+
+#List Comprehensions
+L=[x*x for x in range(1,11)]
+print(L)
